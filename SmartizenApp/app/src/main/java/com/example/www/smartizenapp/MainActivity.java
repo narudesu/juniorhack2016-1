e22a9dec7afbf0a0560b2766c735f8ca36982ad2
František Běhal <Frantiek Bhal>
2016-12-03 00:39:47 +0100

    Vývoj Android aplikace

--- SmartizenApp/app/src/main/java/com/example/www/smartizenapp/MainActivity.java
+++ SmartizenApp/app/src/main/java/com/example/www/smartizenapp/MainActivity.java
@@ -1,26 +1,27 @@
 package com.example.www.smartizenapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
-import android.widget.EditText;
+import android.content.Intent;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    public void locationMap(View view)
+    {
+        Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
 
     }
-
 }
