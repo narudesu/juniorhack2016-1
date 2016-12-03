d373d37ddbbbdc11aec376ddc1d7928f491ac175
dominik <me@domiz.cz>
2016-12-03 02:29:03 +0100

    WEB DEV STARTED

---
+++ web/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wepčí</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="custom.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<nav class="navbar navbar-default" id="menu" style="margin-bottom: 0px;">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Smarti<span class="gray">ZEN</span></a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+        </ul>
+    </div>
+</nav>
+<section class="top">
+    <div class="imagecentered"></div>
+</section>
+<section class="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <h1>About SmartiZEN</h1>
+                <h3>With Smarti<span style class="red">ZEN</span> renting a bike could not be easier.<br> This simple yet efficent solution allows you to <br><span style class="red">rent a bike</span> from across the city and ride it to your desired location.</h3>
+                <ul>
+                    <li>Rent a bike for up to 8 hours</li>
+                    <li>Accessible from anywhere</li>
+                    <li>Includes alhocol test</li>
+                    <li>5€ per bike</li>
+                </ul>
+            </div>
+            <div class="col-lg-4">
+                <div class="imageabout">
+                    <img src="http://placehold.it/400x400" style="border-radius: 5px">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="rent">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h1>Request a bike</h1>
+                <p>Request a bike either from your computer or a mobile phone and receive nearest station location.
+                When you are finished with your ride, just park it at the nearest station.</p>
+            </div>
+            <div class="col-lg-6">
+                <div>
+                    <h1>Pricing</h1>
+                    <ul>
+                        <li>Bike rental fee - 5€</li>
+                        
+                    </ul>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
