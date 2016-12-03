8d9d2a0e63818dba8cfb86d3b77e59dedb53261e
narudesu <narudesu@yahoo.com>
2016-12-03 11:11:15 +0100

    php is evil

--- web/index.php
+++ web/index.php
@@ -7,10 +7,37 @@
     <title>SmartiZEN</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="custom.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" charset="utf-8"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <style>
+      #mapsection {
+        min-height: 500px;
+        width: 100%;
+      }
+
+      #bike-finder {
+        min-height: 200px;
+        transition: all 1s ease-in-out;
+      }
+
+      #bike-finder-map {
+        height: 200px;
+        width: 100%;
+      }
+
+      .collapsed {
+        transform: scaleY(0);
+      }
+
+      .absrender {
+        position: absolute;
+      }
+    </style>
 </head>
+
+
 
 <body>
 <script type="text/javascript">
@@ -111,15 +138,102 @@
         <p style="float: right;">* For every half hour spent after plan due</p>
     </div>
 </section>
+<section id="mapsection">
+  <h2>Get kolo</h2>
+  <button id="btn-kolo">Kolo</button>
+  <div id="bike-finder" class="collapsed absrender">
+    <div class="row">
+      <div class="col-md-6">
+        <div id="bike-finder-map"></div>
+      </div>
+      <div class="col-md-6">
+        <table id="bike-finder-table" class="table">
+          <tr>
+            <th>ID</th>
+            <th>Distance</th>
+            <th>Slots</th>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione eos minima voluptates accusantium praesentium molestias porro vero vitae, laboriosam eligendi?</p>
+</section>
 <!-- HERE IS A SPACE FOR THE MAP -->
 <section class="creators" id="creators">
     <div class="container">
         <div class="row">
             <h1  class="X300">About the creators</h1>
+            <div class="col-lg-6 col-md-12 card col-xs-12">
+                        <div class="photo">
+                            <img src="http://placehold.it/200x200">
+                        </div>
+                        <div class="crea_content">
+                            <h5>Dominik <span class="lastname">Kadera</span></h5>
+                            <h6></h6>
+                            <p><img src="Photos/1480739606_social-facebook-square2.png" height="32px"></p>
+                            <p><img src="Photos/1480739674_gmail.png"  height="32px"></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 card col-xs-12">
+                                <div class="photo">
+                                    <img src="http://placehold.it/200x200">
+                                </div>
+                                <div class="crea_content">
+                                    <h5>Vojtech <span class="lastname">Bohm</span></h5>
+                                    <h6>'.$post.'</h6>
+                                    <p><img src="Photos/1480739606_social-facebook-square2.png" height="32px"> '.$facebook.'</p>
+                                    <p><img src="Photos/1480739674_gmail.png"  height="32px"> '.$mail.'</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-12 card col-xs-12">
+                                        <div class="photo">
+                                            <img src="http://placehold.it/200x200">
+                                        </div>
+                                        <div class="crea_content">
+                                            <h5>'.$jmeno.' <span class="lastname">'.$prijmeni.'</span></h5>
+                                            <h6>'.$post.'</h6>
+                                            <p><img src="Photos/1480739606_social-facebook-square2.png" height="32px"> '.$facebook.'</p>
+                                            <p><img src="Photos/1480739674_gmail.png"  height="32px"> '.$mail.'</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12 card col-xs-12">
+                                                <div class="photo">
+                                                    <img src="http://placehold.it/200x200">
+                                                </div>
+                                                <div class="crea_content">
+                                                    <h5>'.$jmeno.' <span class="lastname">'.$prijmeni.'</span></h5>
+                                                    <h6>'.$post.'</h6>
+                                                    <p><img src="Photos/1480739606_social-facebook-square2.png" height="32px"> '.$facebook.'</p>
+                                                    <p><img src="Photos/1480739674_gmail.png"  height="32px"> '.$mail.'</p>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6 col-md-12 card col-xs-12">
+                                                        <div class="photo">
+                                                            <img src="http://placehold.it/200x200">
+                                                        </div>
+                                                        <div class="crea_content">
+                                                            <h5>'.$jmeno.' <span class="lastname">'.$prijmeni.'</span></h5>
+                                                            <h6>'.$post.'</h6>
+                                                            <p><img src="Photos/1480739606_social-facebook-square2.png" height="32px"> '.$facebook.'</p>
+                                                            <p><img src="Photos/1480739674_gmail.png"  height="32px"> '.$mail.'</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-6 col-md-12 card col-xs-12">
+                                                                <div class="photo">
+                                                                    <img src="http://placehold.it/200x200">
+                                                                </div>
+                                                                <div class="crea_content">
+                                                                    <h5>'.$jmeno.' <span class="lastname">'.$prijmeni.'</span></h5>
+                                                                    <h6>'.$post.'</h6>
+                                                                    <p><img src="Photos/1480739606_social-facebook-square2.png" height="32px"> '.$facebook.'</p>
+                                                                    <p><img src="Photos/1480739674_gmail.png"  height="32px"> '.$mail.'</p>
+                                                                </div>
+                                                            </div>
             <?php
             echo makeCard("http://placehold.it/200x200","Dominik","Kadera","Leader, Hardware, Automatization","me@domiz.cz","facebook.com/dominik.kadera");
             echo makeCard("http://placehold.it/200x200","Vojtěch","Böhm","Graphic design & Front-end development","vojta.bohm@gmail.com","facebook.com/vojta.bohm");
-            echo makeCard("http://placehold.it/200x200","Matěj","Šmíd","Database Management, Front & Back-end development","facebook","mejlik");
+            echo makeCard("http://placehold.it/200x200","Matěj","Šmíd","Database Management, Full-stack web development","facebook","narudesu@yahoo.com");
             echo makeCard("http://placehold.it/200x200","Jan","Šoulák","Hardware, communication","mejlik","facebook");
             echo makeCard("http://placehold.it/200x200","František","Běhal","Android deployment","mejlik","facebook");
             echo makeCard("http://placehold.it/200x200","David","Baňař","Hardware","mejlik","facebook");
@@ -130,5 +244,30 @@
 <footer>
     <div class="koudelka" style="background-color: black; width: 100%; height: 50px"></div>
 </footer>
+<div id="modal-info" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>
+            Distance from user: <span class="distance-from-user"></span>
+            Rental fee: <span class="rental-fee"></span>
+            Slot state:
+            <button>Rent</button>
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
 </body>
 </html>
